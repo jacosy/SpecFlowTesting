e7aa44beb2fdda233533cd4ba2ae4568366b1c11
Longoria.You <longoria.you@gmail.com>
2018-10-07 13:38:26 +0800

    Revert "Made Changes For Personal Latop only"
    This reverts commit e18720620d10b719fe29269430d9427763b6cd41.

--- _gui/version/VersionInfo.cs
+++ _gui/version/VersionInfo.cs
@@ -2,9 +2,10 @@
 // this file should be added to all projects
 
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyCompany("FutureVision Technologies, LLC")]
-[assembly: AssemblyProduct("Catalyst?")]
-[assembly: AssemblyCopyright("Copyright 2005-2006 ?FutureVision Technologies, LLC")]
-[assembly: AssemblyTrademark("Catalyst?is a trademark of FutureVision Technologies, LLC")]
+[assembly: AssemblyProduct("Catalyst™")]
+[assembly: AssemblyCopyright("Copyright 2005-2006 © FutureVision Technologies, LLC")]
+[assembly: AssemblyTrademark("Catalyst™ is a trademark of FutureVision Technologies, LLC")]
 [assembly: AssemblyVersion("1.7.1715.0")]
