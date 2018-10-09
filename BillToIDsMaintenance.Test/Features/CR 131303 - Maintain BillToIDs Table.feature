2e7229ef8ad679ae54d316ff2a4dc1202d77f64c
Longoria.Yu <Longoria.Yu@innovasolutions.com>
2018-10-09 16:58:07 +0800

    Finish the spec and the unit tests

---
+++ BillToIDsMaintenance.Test/Features/CR 131303 - Maintain BillToIDs Table.feature
@@ -0,0 +1,51 @@
+﻿@CR 131303 @BillToID @IBM MSC Outbound Routing
+
+Feature: CR 131303 - Maintain BillToIDs Table
+	IBM MSC Outbound Routing - create tool in Strategic EDI tool so that we can modify the table for CR 131302 at any time by adding and removing bill to IDs
+	In order to maintain BillToIDs table in UnitedStationers DB
+	As an operator
+	I want to have a functionality to add or delete BillToID in BillToIDs table
+
+Scenario: Get BillToIDs
+	When User does not provide any Keyword to search BillToID
+	Then It should return all the BillToIDs
+
+Scenario Outline: Search BillToIDs
+	When User use <Keyword> to search BillToID
+	Then It should return the <ExpectedRecords> records containing the keyword
+
+	Examples: 
+	| Keyword | ExpectedRecords |
+	| ABC     | 3               |
+	| 987     | 1               |
+	| ****    | 0               |
+
+Scenario Outline: Creat a New BillToID
+	When User tries to create a <BillToID>
+		And There is no matched <BillToID> in DB
+	Then The <BillToID> should be created and stored in BillToIDs table
+
+	Examples: 
+	| BillToID |
+	| 987654   |
+	| NewID    |
+
+Scenario Outline: Try to Create an existing BillToID
+	When User tries to create a <BillToID>
+		And There is a matched <BillToID> in DB
+	Then The <BillToID> should not be created and stored in BillToIDs table
+	
+	Examples: 
+	| BillToID |
+	| 123456   |
+	| ABCEDF   |
+
+Scenario Outline: Delete an existing BillToID
+	Given There is an existing <BillToID> in BillToID table
+	When User tries to delete <BillToID>
+	Then <BillToID> should be deleted from BillToID table
+
+	Examples: 
+	| BillToID |
+	| 123456   |
+	| ABCEDF   |
---
+++ BillToIDsMaintenance.Test/MaintainBillToIDsTableSteps.cs
@@ -0,0 +1,128 @@
+﻿using BillToIDsMaintenance.Models;
+using FvTech.Data;
+using Moq;
+using NUnit.Framework;
+using System.Data;
+using TechTalk.SpecFlow;
+
+namespace BillToIDsMaintenance.Test
+{
+    [Binding]
+    public class MaintainBillToIDsTableSteps
+    {
+        BMPresenter sut;
+        IBMViewer viewer;        
+        SqlDatabaseConnection databaseConnection;
+        string userName;        
+
+        public MaintainBillToIDsTableSteps()
+        {
+            databaseConnection = new SqlDatabaseConnection(@"Initial Catalog=UnitedStationers;Data Source=(localdb)\MSSQLLocalDB;Integrated Security=true;");
+            userName = "Longoria";
+            var moqViewer = new Mock<IBMViewer>();
+            moqViewer.Setup(view => view.Show(new BMViewModel()));
+            viewer = moqViewer.Object;
+            sut = new BMPresenter(databaseConnection, userName, viewer);
+        }
+
+        #region Scenario: Get BillToIDs
+        [When(@"User does not provide any Keyword to search BillToID")]
+        public void WhenUserDoesNotProvideAnyKeywordToSearchBillToID()
+        {
+            sut.Create("123456");
+            sut.Create("ABCEDF");
+            sut.Create("poiuy");
+        }
+
+        [Then(@"It should return all the BillToIDs")]
+        public void ThenItShouldReturnAllTheBillToIDs()
+        {
+            DataTable dt = sut.Search();
+            Assert.That(dt.Rows, Has.Count.EqualTo(3));
+            databaseConnection.RunSqlCommand("truncate table BillToIDs");
+        }
+        #endregion
+
+        #region Scenario Outline: Delete an existing BillToID
+        [Given(@"There is an existing (.*) in BillToID table")]
+        public void GivenThereIsAnExistingInBillToIDTable(string billToID)
+        {           
+            string msg = sut.Create(billToID);
+        }
+
+        [When(@"User tries to delete (.*)")]
+        public void WhenUserTriesToDelete(string billToID)
+        {
+            sut.Delete(billToID);
+        }
+
+        [Then(@"(.*) should be deleted from BillToID table")]
+        public void ThenShouldBeDeletedFromBillToIDTable(string billToID)
+        {
+            DataTable dt = sut.Search(billToID);
+            Assert.That(dt.Rows, Has.Count.EqualTo(0));
+            databaseConnection.RunSqlCommand("truncate table BillToIDs");
+        }
+        #endregion                               
+
+        #region Scenario Outlines: 1. Creat a New BillToID; 2. Try to Create an existing BillToID
+        [When(@"User tries to create a (.*)")]
+        public void WhenUserTriesToCreateBillToID(string billToID)
+        {
+            sut.Create(billToID);
+        }
+        
+        [When(@"There is no matched (.*) in DB")]
+        public void WhenThereIsNoMatchedInDB(string billToID)
+        {           
+        }
+
+        [Then(@"The (.*) should be created and stored in BillToIDs table")]
+        public void BillToIdShouldBeCreatedAndStoredInBillToIDsTable(string billToID)
+        {
+            DataTable dt = sut.Search(billToID);
+            Assert.That(dt.Rows, Has.Count.EqualTo(1));
+            databaseConnection.RunSqlCommand("truncate table BillToIDs");
+        }
+
+        [When(@"There is a matched (.*) in DB")]
+        public void WhenThereIsAMatchedInDB(string billToID)
+        {
+            sut.Create(billToID);
+        }
+
+        [Then(@"The (.*) should not be created and stored in BillToIDs table")]
+        public void BillToIdShouldNotBeCreatedAndStoredInBillToIDsTable(string billToID)
+        {
+            DataTable dt = sut.Search(billToID);
+            Assert.That(dt.Rows, Has.Count.EqualTo(1));
+            databaseConnection.RunSqlCommand("truncate table BillToIDs");
+        }
+        #endregion
+
+        #region Scenario Outline: Search BillToIDs      
+        [Given(@"There are the following records in BillToIDs table: ABC123, ABC456, ABC987 & Test")]
+        public void GivenThereAreTheFollowingRecordsInBillToIDsTableABCABCABCTest()
+        {
+            sut.Create("ABC123");
+            sut.Create("ABC456");
+            sut.Create("ABC987");
+            sut.Create("Test");
+        }
+
+        [When(@"User use (.*) to search BillToID")]
+        public void WhenUserUseABCToSearchBillToID(string keyword)
+        {
+            DataTable dt = sut.Search(keyword);
+            ScenarioContext.Current.Add("Result", dt.Rows.Count);
+        }
+
+        [Then(@"It should return the (.*) records containing the keyword")]
+        public void ThenItShouldReturnTheRecordsContainingTheKeyword(int expectedRecords)
+        {
+            Assert.That(int.Parse(ScenarioContext.Current["Result"].ToString()) == expectedRecords);
+            databaseConnection.RunSqlCommand("truncate table BillToIDs");
+        }
+        #endregion
+    }
+}
