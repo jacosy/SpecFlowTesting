5d720e7d467c9638b1ccbc358c80a09f1dd13f58
Longoria.You <longoria.you@gmail.com>
2018-10-13 14:47:14 +0800

    replaced "Catalyst by "Catalyst"

--- _gui/version/VersionInfo.cs
+++ _gui/version/VersionInfo.cs
@@ -2,10 +2,9 @@
 // this file should be added to all projects
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
 [assembly: AssemblyCompany("FutureVision Technologies, LLC")]
-[assembly: AssemblyProduct("Catalyst™")]
-[assembly: AssemblyCopyright("Copyright 2005-2006 © FutureVision Technologies, LLC")]
-[assembly: AssemblyTrademark("Catalyst™ is a trademark of FutureVision Technologies, LLC")]
+[assembly: AssemblyProduct("Catalyst")]
+[assembly: AssemblyCopyright("Copyright 2005-2006 ?FutureVision Technologies, LLC")]
+[assembly: AssemblyTrademark("Catalyst?is a trademark of FutureVision Technologies, LLC")]
 [assembly: AssemblyVersion("1.7.1715.0")]
